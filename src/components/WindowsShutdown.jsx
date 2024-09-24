a80a1925b56cec556077572530b741c1914cf29d
Yute Lilitprapun <yudthsoponvit@gmail.com>
2024-09-24 08:36:17 -0400

    .

--- src/components/WindowsShutdown.jsx
+++ src/components/WindowsShutdown.jsx
@@ -7,7 +7,7 @@ function WindowsShutdown() {
     <div className='shitdown_bg_container'>
         <img src={windowsshut} alt="windowsShutdown" />
         <h1 className="shutdown_text">
-            It is now safe to turn off your computer.
+            It is now safe to turn off<br />your computer.
         </h1>
     </div>
   )
