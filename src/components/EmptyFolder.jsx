db64f35804569c1be5e4a82c16c0266a063f036b
Yute Lilitprapun <yudthsoponvit@gmail.com>
2025-01-29 21:31:44 -0500

    added picture folder added prop to create new folder easily

---
+++ src/components/EmptyFolder.jsx
@@ -0,0 +1,246 @@
+import UseContext from '../Context';
+import { Fragment, useContext, useEffect, useRef, useState} from "react";
+import Draggable from 'react-draggable';
+import { motion } from 'framer-motion';
+import '../css/ResumeFolder.css';
+import PropTypes from 'prop-types';
+
+function EmptyFolder({state, setState, refState, folderName}) {
+
+  const iconRefs = useRef([]);
+
+  const { 
+    iconContainerSize, iconImgSize, iconTextSize,
+    iconScreenSize,
+    key,
+    dragging,
+    handleOnDrag,
+    handleDrop,
+    dropTargetFolder, setDropTargetFolder,
+    imageMapping,
+    desktopIcon, 
+    themeDragBar,
+    lastTapTime, setLastTapTime,
+    StyleHide,
+    isTouchDevice,
+    handleShowMobile,
+    handleShow,
+    handleSetFocusItemTrue,
+    inlineStyleExpand,
+    inlineStyle,
+    iconFocusIcon,
+    deleteTap,
+  } = useContext(UseContext);
+
+  function handleDragStop(event, data) {
+    const positionX = data.x;
+    const positionY = data.y;
+    setState(prev => ({
+      ...prev,
+      x: positionX,
+      y: positionY,
+    }));
+  }
+
+  function handleExpandStateToggle() {
+    setState(prevState => ({
+      ...prevState,
+      expand: !prevState.expand,
+    }));
+  }
+
+  function handleExpandStateToggleMobile() {
+    const now = Date.now();
+    if (now - lastTapTime < 300) {
+      setState(prevState => ({
+        ...prevState,
+        expand: !prevState.expand,
+      }));
+    }
+    setLastTapTime(now);
+  }
+
+  return (
+    <Draggable
+      axis="both" 
+      handle={'.folder_dragbar'}
+      grid={[1, 1]}
+      scale={1}
+      disabled={state.expand}
+      bounds={{ top: 0 }}
+      defaultPosition={{ 
+        x: window.innerWidth <= 500 ? 30 : 60,
+        y: window.innerWidth <= 500 ? 30 : 80,
+      }}
+      onStop={(event, data) => {handleDragStop(event, data)}}
+      onStart={() => {handleSetFocusItemTrue(folderName)}}
+    >
+        <motion.div 
+          ref={refState}
+          className='folder_folder'
+          onClick={(e) => {
+            e.stopPropagation();
+          handleSetFocusItemTrue(folderName);
+          }}
+          style={{
+            ...(
+                state.expand
+                    ? inlineStyleExpand(folderName)
+                    : inlineStyle(folderName)
+            ),
+            overflow: dragging ? '' : 'hidden'
+        }}
+        >
+        <div className="folder_dragbar"
+          onDoubleClick={handleExpandStateToggle}
+          onTouchStart={handleExpandStateToggleMobile}
+          style={{ background: state.focusItem ? themeDragBar : '#757579' }}
+        >
+          <div className="folder_barname">
+            <img src={imageMapping(folderName)} alt="folderName" />
+            <span>{folderName}</span>
+          </div>
+          <div className="folder_barbtn">
+            <div onClick={ !isTouchDevice ? (e) => {
+              e.stopPropagation();
+              setState(prev => ({ ...prev, hide: true, focusItem: false }));
+              StyleHide(folderName); 
+            } : undefined}
+            onTouchEnd={(e) => {
+              e.stopPropagation()
+              setState(prev => ({...prev, hide: true, focusItem: false}))
+              StyleHide(folderName)
+            }}
+            >
+              <p className='dash'></p>
+            </div>
+            <div 
+              onClick={ !isTouchDevice ? handleExpandStateToggle : undefined}
+              onTouchEnd={handleExpandStateToggleMobile}
+            >
+              <motion.div className={`expand ${state.expand ? 'full' : ''}`} />
+              {state.expand ? <div className="expand_2"></div> : null}
+            </div>
+            <div>
+              <p className='x'
+                onClick={!isTouchDevice ? () => deleteTap(folderName) : undefined}
+                onTouchEnd={() => deleteTap(folderName)}
+              >×</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="file_edit_container">
+          <p>File<span style={{ left: '-23px' }}>_</span></p>
+          <p>Edit<span style={{ left: '-24px' }}>_</span></p>
+          <p>View<span style={{ left: '-32px' }}>_</span></p>
+          <p>Help<span style={{ left: '-30px' }}>_</span></p>
+        </div>
+        <div className="folder_content"
+          onClick={() => iconFocusIcon('')}
+          style={{ 
+            height: state.expand ? 'calc(100svh - 122px)' : '',
+            overflow: dragging? '' : 'hidden' 
+          }}
+
+
+        >
+          <div className='parent_item_container' key={key}
+
+          >
+            <div className="item_container" 
+              style={{
+                position: dragging? 'absolute' : '',
+              }}
+              onClick={(e) => e.stopPropagation()}
+              >
+              {desktopIcon.filter(icon => icon.folderId === folderName).map(icon => (
+                <Fragment key={icon.name}>
+                  <Draggable
+                  axis="both" 
+                  handle={'.icon'}
+                  grid={[10, 10]}
+                  scale={1}
+                  bounds={false}
+                  onStart={() => {
+                    setDropTargetFolder('')
+                    handleSetFocusItemTrue(folderName)
+                  }}
+                  onDrag={handleOnDrag(icon.name, iconRefs.current[icon.name])}
+                  onStop={(e) => {
+                    handleDrop(e, icon.name, dropTargetFolder);
+                  }}
+                >
+                  <div className='icon' key={icon.name}
+                    style={iconContainerSize(iconScreenSize)}
+                    ref={(el) => iconRefs.current[icon.name] = el}
+                    onDoubleClick={() => handleShow(icon.name)}                      
+                    onClick={!isTouchDevice ? (e) => {
+                      iconFocusIcon(icon.name);
+                      e.stopPropagation();
+                    } : undefined}           
+                    onTouchStart={() => {
+                      handleShowMobile(icon.name);
+                      iconFocusIcon(icon.name);
+                    }}
+                  >
+                    <img src={imageMapping(icon.name)} alt='#' className={icon.focus ? 'img_focus' : ''}
+                      style={iconImgSize(iconScreenSize)}
+                    />
+                    <p className={icon.focus ? 'p_focus' : 'p_normal'}
+                      style={iconTextSize(iconScreenSize)}
+                    >
+                      {icon.name}
+                    </p>
+                  </div>
+                  </Draggable>
+                </Fragment>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="btm_bar_container">
+          <div className="object_bar">
+            <p>
+              {desktopIcon.filter(icon => icon.folderId === folderName).some(icon => icon.focus) ? 
+                '1 object(s) selected'
+                :
+                desktopIcon.filter(icon => icon.folderId === folderName).length + ' ' + 'object(s)'
+              }
+            </p>
+          </div>
+          <div className="size_bar">
+            <p>
+              {(() => {
+                const filteredIcons = desktopIcon.filter(icon => icon.folderId === folderName);
+                const totalSize = filteredIcons.reduce((total, icon) => total + icon.size, 0);
+                const allNotFocused = filteredIcons.every(icon => !icon.focus);
+
+                if (allNotFocused) {
+                  return totalSize; 
+                } else {
+                  return filteredIcons
+                    .filter(icon => icon.focus)
+                    .map(icon => icon.size) 
+                    .reduce((sum, size) => sum + size, 0); 
+                }
+              })()} KB
+            </p>
+          </div>
+        </div>
+      </motion.div>
+    </Draggable>
+  );
+}
+EmptyFolder.propTypes = {
+  state: PropTypes.shape({
+    expand: PropTypes.bool.isRequired,
+    focusItem: PropTypes.bool,
+    hide: PropTypes.bool,
+  }).isRequired,
+  setState: PropTypes.func.isRequired,
+  refState: PropTypes.object,
+  folderName: PropTypes.string.isRequired,
+};
+
+export default EmptyFolder;
